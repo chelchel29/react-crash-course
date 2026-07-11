d2b6190153e75ee44ceb428aeda539b5be964089
jiji <yooncherry2910@gmail.com>
2026-07-11 21:26:47 +0630

    navigate

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { useParams, useSearchParams } from "react-router";
+import { useNavigate, useParams, useSearchParams } from "react-router";
 
 function InfoPage() {
   const { id } = useParams();
   const [serarchParams] = useSearchParams();
   const query = serarchParams.get("query");
   const key = serarchParams.get("key");
+
+  const navigate = useNavigate();
+  const navigateToHome = () => {
+    navigate("/");
+  };
   return (
     <div>
       InfoPage - {id} - {query} - {key}
+      <button onClick={navigateToHome}>click me</button>
     </div>
   );
 }
